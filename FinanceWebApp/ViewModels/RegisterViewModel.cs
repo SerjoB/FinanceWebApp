340aca0d12d900162d7f52d52a33fc0e33408fb2
SerjoB <berestov_s_31@mail.ru>
2025-08-25 04:10:51 +0300

    Account Views

---
+++ FinanceWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceWebApp.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = null!;
+
+    [Required, DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [DataType(DataType.Password), Compare("Password")]
+    public string ConfirmPassword { get; set; } = null!;
+}
